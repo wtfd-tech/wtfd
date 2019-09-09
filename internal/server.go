d9f34c8c9740ec6a99eb5d0fd07f7d39d1576630
Philipp Hochkamp <philipp.hochkamp@udo.edu>
2019-09-09 18:41:00 +0200

    asdf

--- internal/server.go
+++ internal/server.go
@@ -1,10 +1,14 @@
 package wtfd
 
 import (
+	"encoding/gob"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
@@ -13,25 +17,29 @@ import (
 
 var (
 	// key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
-	key             = []byte("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-	store           = sessions.NewCookieStore(key)
-	ErrUserExisting = errors.New("User with this name exists")
+	key                = []byte("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+	store              = sessions.NewCookieStore(key)
+	ErrUserExisting    = errors.New("User with this name exists")
+	ErrWrongPassword   = errors.New("Wrong Password")
 	ErrUserNotExisting = errors.New("User with this name does not exist")
+	users              = Users{}
+	challs             = Challenges{}
 )
 
 type Users []User
+type Challenges []Challenge
 
 type Challenge struct {
-	Title       string
-	Description string
-	Flag        string
-	Points      int
+	Title       string `json:"title"`
+	Description string `json:"desc"`
+	Flag        string `json:"flag"`
+	Points      int    `json:"points"`
 }
 
 type User struct {
 	Name      string
 	Hash      []byte
-	Completed []Challenge
+	Completed []*Challenge
 }
 
 type MainPageData struct {
@@ -41,19 +49,38 @@ type MainPageData struct {
 }
 
 func (u *Users) Contains(username string) bool {
-	for _, user := range *u {
-		if user.Name == username {
-			return true
-		}
-	}
-	return false
+	_, err := u.Get(username)
+	return err != nil
 }
 
-func (u *Users) Get(username string) 
+func (u *Users) Get(username string) (User, error) {
+	for _, user := range *u {
+		if user.Name == username {
+			return user, nil
+		}
+	}
+	return User{}, ErrUserNotExisting
 
+}
+
+func (u *Users) Login(username, passwd string) (User, error) {
+	user, err := u.Get(username)
+	if err != nil {
+		return User{}, err
+	}
+	if pwdRight := user.ComparePassword(passwd); !pwdRight {
+		return User{}, ErrWrongPassword
+	}
+	return user, nil
+
+}
+
+func (u *User) ComparePassword(password string) bool {
+	return bcrypt.CompareHashAndPassword(u.Hash, []byte(password)) == nil
+}
 
 func (u *User) New(name, password string) (User, error) {
-	if Users.Contains(name) {
+	if users.Contains(name) {
 		return User{}, ErrUserExisting
 	}
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), 14)
@@ -67,19 +94,41 @@ func (u *User) New(name, password string) (User, error) {
 
 func mainpage(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "auth")
-        user := Users.Get(session.Values["username"])
+	user, _ := session.Values["User"].(*User)
 
-        data := MainPageData{
-          PageTitle: "foss-ag O-Phasen CTF",
-          Challenges: Challenges,
-          User: User(user),
-        }
+	data := MainPageData{
+		PageTitle:  "foss-ag O-Phasen CTF",
+		Challenges: challs,
+		User:       *user,
+	}
 
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "auth")
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Invalid Request")
 
+	} else {
+		session, _ := store.Get(r, "auth")
+		if session.Values["User"] != "" {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "Already logged in")
+		} else {
+			u, err := users.Login(r.Form["username"], r.Form["password"])
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprintf(w, "Server Error: %v", err)
+			} else {
+				session.Values["User"] = u
+				session.Save(r, w)
+				http.Redirect(w, r, mainpage)
+
+			}
+
+		}
+
+	}
 
 }
 
@@ -88,6 +137,22 @@ func logout(w http.ResponseWriter, r *http.Request) {
 }
 
 func Server() error {
+	gob.Register(&User{})
+
+        // Loading challs file
+	challsFile, err := os.Open("challs.json")
+	if err != nil {
+		return err
+	}
+	defer challsFile.Close()
+	challsFileBytes, _ := ioutil.ReadAll(challsFile)
+	if err := json.Unmarshal(challsFileBytes, &challs); err != nil {
+		return err
+	}
+
+        // Loading template files
+
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", mainpage)
 
