351cb79440fa8419086da5a5273c9d4e4f2a93f8
Philipp Hochkamp <philipp.hochkamp@udo.edu>
2019-09-15 03:27:12 +0200

    added testing to all used orm functions

--- internal/orm.go
+++ internal/orm.go
@@ -167,7 +167,7 @@ func ormUserExists(user User) (bool, error) {
 
 // get Challenges{} solved by user (db-state)
 func ormChallengesSolved(user User) ([]Challenge, error) {
-	challenges := make([]Challenge, 0)
+	var challenges []Challenge
 
 	engine.Where("UserName = ?", user.Name).Iterate(_ORMChallengesByUser{}, func(i int, bean interface{}) error {
 		relation := bean.(*_ORMChallengesByUser)
--- internal/orm_test.go
+++ internal/orm_test.go
@@ -7,17 +7,20 @@ import (
 )
 
 var (
-	u     User = User{Name: "testuser", Hash: []byte("a")}
-	fakeu User = User{Name: "faketestuser", Hash: []byte("a")} // No Challenges because new users dont have any
+	u         User      = User{Name: "testuser", Hash: []byte("a")}
+	fakeu     User      = User{Name: "faketestuser", Hash: []byte("a")} // No Challenges because new users dont have any
+	testchall Challenge = Challenge{Name: "testchall", Flag: "testflag"}
 )
 
 // TestMain tests all the good ol orm stuff
 func TestMain(m *testing.M) {
+	os.Remove("./state.db")
 	err := ormStart("./testdblog")
 	if err != nil {
 		fmt.Printf("Database Creation failed: %v", err)
 		os.Exit(1)
 	}
+	challs = append(challs, testchall)
 	os.Exit(m.Run())
 
 }
@@ -48,6 +51,26 @@ func TestUserExists(t *testing.T) {
 		t.Errorf("Database UserExists failed: %v", fmt.Sprintf("Fake User %v does exist", fakeu))
 	}
 }
+
+func TestUserDeletion(t *testing.T) {
+
+	deleteTestUser := User{Name: "deletetestuser"}
+_:
+	ormNewUser(deleteTestUser)
+	err := ormDeleteUser(deleteTestUser)
+	if err != nil {
+		t.Errorf("Database UserDelete failed: %v", err)
+	}
+
+	hopefullyFalse, err := ormUserExists(deleteTestUser)
+	if err != nil {
+		t.Errorf("Database UserDelete failed: %v", err)
+	}
+	if hopefullyFalse {
+		t.Errorf("Database UserDelete failed: %v", fmt.Sprintf("Deleted User %v does exist", deleteTestUser))
+	}
+}
+
 func TestPullingUserFromDatabase(t *testing.T) {
 
 	uFromDB, err := ormLoadUser(u.Name)
@@ -74,4 +97,26 @@ func TestDeletingUserFromDatabase(t *testing.T) {
 		t.Errorf("Database UserDelete failed: %v", fmt.Sprintf("Deleted User %v does exist", u))
 	}
 
+}
+
+func TestUserSolvesChallenge(t *testing.T) {
+
+	solvetestuser := User{Name: "asdf"}
+	_ = ormNewUser(solvetestuser)
+	err := ormSolvedChallenge(solvetestuser, testchall)
+	if err != nil {
+		t.Errorf("Database Challenge Solve failed: %v", err)
+	}
+	uFromDB, err := ormLoadUser(solvetestuser.Name)
+	fmt.Printf("%+v", uFromDB)
+	if err != nil {
+		t.Errorf("Database Challenge Solve failed: %v", err)
+	}
+	if !(uFromDB.Completed[0].Name == testchall.Name) {
+
+		t.Errorf("Database Challenge Solve failed: %v",
+			fmt.Sprintf("uFromDB.Completed[0].Name == testchall.Name, but uFromDB.Completed[0].Name = %v",
+				uFromDB.Completed[0].Name))
+
+	}
 }
